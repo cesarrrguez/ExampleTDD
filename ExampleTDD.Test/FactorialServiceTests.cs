7e7f107dc7baeab6fea3d08ea2be73e21108a35c
César Rodríguez <cesar.rrguez@gmail.com>
2020-07-09 10:50:23 +0200

    Use Arrange, Act and Assert (AAA) Pattern for tests

--- ExampleTDD.Test/FactorialServiceTests.cs
+++ ExampleTDD.Test/FactorialServiceTests.cs
@@ -7,20 +7,26 @@ namespace ExampleTDD.Test
         [Fact]
         public void TestFactorialZero()
         {
+            // Arrange
             var service = new FactorialService();
 
+            // Act
             var result = service.Calculate(0);
 
+            // Assert
             Assert.Equal(1, result);
         }
 
         [Fact]
         public void TestFactorialFive()
         {
+            // Arrange
             var service = new FactorialService();
 
+            // Act
             var result = service.Calculate(5);
 
+            // Assert
             Assert.Equal(120, result);
         }
     }
