2699ae0ca7ef1f4f153c3808d27dcabc4f8040d3
César Rodríguez <cesar.rrguez@gmail.com>
2020-07-09 10:49:17 +0200

    Add test project with Factorial Service tests

---
+++ ExampleTDD.Test/FactorialServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace ExampleTDD.Test
+{
+    public class FactorialServiceTests
+    {
+        [Fact]
+        public void TestFactorialZero()
+        {
+            var service = new FactorialService();
+
+            var result = service.Calculate(0);
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void TestFactorialFive()
+        {
+            var service = new FactorialService();
+
+            var result = service.Calculate(5);
+
+            Assert.Equal(120, result);
+        }
+    }
+}
