c1fe2c2d0774f81a9a9f9a946ec7c760b2191bb6
César Rodríguez <cesar.rrguez@gmail.com>
2020-07-09 10:51:06 +0200

    Add theory test method

--- ExampleTDD.Test/FactorialServiceTests.cs
+++ ExampleTDD.Test/FactorialServiceTests.cs
@@ -29,5 +29,20 @@ namespace ExampleTDD.Test
             // Assert
             Assert.Equal(120, result);
         }
+
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(5, 120)]
+        public void TestFactorial(int number, int expected)
+        {
+            // Arrange
+            var service = new FactorialService();
+
+            // Act
+            var result = service.Calculate(number);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
